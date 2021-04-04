6dc4b5128282d4c763231beb1e6896545a746fc9
Erik Ivanov <erik.ivanov.n@gmail.com>
2021-04-03 23:59:17 -0600

    if the token has expired

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:provider/provider.dart';
+import 'package:users_publications/pages/home_page.dart';
 import 'package:users_publications/pages/init_page.dart';
 import 'package:users_publications/pages/login_page.dart';
 
@@ -22,8 +24,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final String initialRoute =
-        userPreferences.init ? InitPage.name : LoginPage.name;
+    final tokenHasExpired = userPreferences.token == ''
+        ? true
+        : JwtDecoder.isExpired(userPreferences.token);
+
+    final String initialRoute = userPreferences.init
+        ? InitPage.name
+        : tokenHasExpired
+            ? LoginPage.name
+            : HomePage.name;
 
     return MaterialApp(
       theme: Provider.of<ThemeChanger>(context).currentTheme,
